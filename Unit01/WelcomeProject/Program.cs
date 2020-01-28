db3a96c6a49390d957b02b997ccb6deb8a9590c2
Catie H <41378408+hatuhsawl@users.noreply.github.com>
2020-01-28 11:07:21 -0600

    Add files via upload

---
+++ Unit01/WelcomeProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/**
+ * @author Catie Hattesohl
+ * Class Program displays a welcome messageto the console window. */           
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to C#!");
+    }
+}
